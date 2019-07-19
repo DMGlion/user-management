761c26bbd8af20d4ba4586b23ddb41bc2d0a8701
David Moya <davidmgarcia6@gmail.com>
2019-07-19 01:48:18 +0100

    Spring Security implemented

--- src/main/java/com/david/serverusermanagement/controller/UserController.java
+++ src/main/java/com/david/serverusermanagement/controller/UserController.java
@@ -22,27 +22,28 @@ public class UserController {
 
     @PostMapping("/api/user/registration")
     public ResponseEntity<?> register(@RequestBody User user){
-         if (userService.findByUsername(user.getUsername()) != null){
+        if(userService.findByUsername(user.getUsername())!=null){
             return new ResponseEntity<>(HttpStatus.CONFLICT);
-         }
-
-         user.setRole(Role.USER);
-         userService.save(user);
-         return new ResponseEntity<>(user, HttpStatus.CREATED);
+        }
+        user.setRole(Role.USER);
+        userService.save(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
     @GetMapping("/api/user/login")
     public ResponseEntity<?> auth(Principal principal){
-        if(principal!=null || principal.getName()== null){
+        if(principal==null || principal.getName()==null){
             return ResponseEntity.ok(principal);
         }
-
         return new ResponseEntity<>(
-                userService.findByUsername(principal.getName()), HttpStatus.OK);
+                userService.findByUsername(principal.getName()),
+                HttpStatus.OK);
     }
 
     @GetMapping("/api/user/all")
     public ResponseEntity<?> getAllUsers(){
+
         return ResponseEntity.ok(userService.findAllUsers());
     }
 }
+
