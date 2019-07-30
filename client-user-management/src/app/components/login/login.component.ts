a4838abafad64105acef4c7c45b0181cb62b7683
David Moya <davidmgarcia6@gmail.com>
2019-07-30 11:12:46 +0100

    Angular routing

--- client-user-management/src/app/components/login/login.component.ts
+++ client-user-management/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import {User} from "../../model/user";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: User = new User();
+  errorMessage:string;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  login(){
+    this.authService.login(this.user).subscribe(data => {
+      this.router.navigate(['/profile']);
+    },err =>{
+      this.errorMessage ="Username or password is incorrect";
+    });
   }
 
 }
