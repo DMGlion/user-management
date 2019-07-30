8aae7bf6bd445557673325ca53e517637416d55e
David Moya <davidmgarcia6@gmail.com>
2019-07-30 12:40:24 +0100

    Loging controller

--- client-user-management/src/app/components/profile/profile.component.ts
+++ client-user-management/src/app/components/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import {User} from "../../model/user";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  currentUser: User;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
 
   ngOnInit() {
+  }
+
+  logOut(){
+    this.authService.logOut()
+      .subscribe(data =>{
+        this.currentUser = null;
+        this.router.navigate(['/login']);
+      },err => {
+
+      });
   }
 
 }
